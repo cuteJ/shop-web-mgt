62f07a790b6beb6701b9069e8058c75e0d893ff1
lixingping <lixingping233@gmail.com>
2020-06-08 11:21:29 +0800

    Fix api url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,10 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"https://www.zzhjshop.com/shop_test"',
-  APP_CONFIG: {
-    name: '"生产环境应用"',
-    baseUrl: '"https://www.zzhjshop.com/shop_test"',
-    wsUrl: '"wss://www.zzhjshop.com/shop_test/ws"'
+  BASE_API: '"http://shop-api.onlythinking.com/shop_test"',
+  APP_CONFIG:{
+    name:'"生产环境应用"',
+    baseUrl: '"http://shop-api.onlythinking.com/shop_test"',
+    wsUrl: '"ws://shop-api.onlythinking.com/shop_test/ws"'
   }
 }
--- config/sit.env.js
+++ config/sit.env.js
@@ -1,10 +1,10 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sit"',
-  BASE_API: '"https://www.zzhjshop.com/shop_test"',
+  BASE_API: '"http://shop-api.onlythinking.com/shop_test"',
   APP_CONFIG:{
     name:'"测试环境应用"',
-    baseUrl: '"https://www.zzhjshop.com/shop_test"',
-    wsUrl: '"wss://www.zzhjshop.com/shop_test/ws"'
+    baseUrl: '"http://shop-api.onlythinking.com/shop_test"',
+    wsUrl: '"ws://shop-api.onlythinking.com/shop_test/ws"'
   }
 }
